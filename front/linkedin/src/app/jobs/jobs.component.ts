d5db33eb532f6f2396d7241f16f48f155336b856
Kostas Evangelou <kostas.ev79@gmail.com>
2018-09-09 00:29:17 +0300

    ff
    fgf

--- front/linkedin/src/app/jobs/jobs.component.ts
+++ front/linkedin/src/app/jobs/jobs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-jobs',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jobs.component.css']
 })
 export class JobsComponent implements OnInit {
-
-  constructor() { }
+  closeResult: string;
+  editjb = {};
+  public jobs = [
+    {title: 'C Developer' , description: 'Young and wild and free c developer with a car and available for breakable swift' } ,
+    {title: 'C2 Developer' , description: 'Young and wild and free c developer with aasdasdsda car and available for breakable swift' } ,
+    {title: 'TCL Developer' , description: 'Young and wild and free c developer with a car assssssssndasdasdasdsdasda available for breakable swift' } ,
+    {title: 'C# Developer' , description: 'Young and wild and free c develorrper with a car anbreakable swift'} ] ;
+  constructor(
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit() {
+  }
+
+  open(content, type) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
--- front/linkedin/src/app/messages/messages.component.ts
+++ front/linkedin/src/app/messages/messages.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
-
+  msgUsers = [
+    { name: 'kostas' , surname: 'evaggelou', id: 1 },
+    { name: 'nik' , surname: 'evaggelou', id: 2 },
+    { name: 'lid' , surname: 'evaggelou', id: 3 },
+    { name: 'kostas' , surname: 'evaggelou', id: 1 },
+    { name: 'nik' , surname: 'evaggelou', id: 2 },
+    { name: 'lid' , surname: 'evaggelou', id: 3 },
+    { name: 'kostas' , surname: 'evaggelou', id: 1 },
+    { name: 'nik' , surname: 'evaggelou', id: 2 },
+    { name: 'lid' , surname: 'evaggelou', id: 3 },
+    { name: 'kostas' , surname: 'evaggelou', id: 1 },
+    { name: 'nik' , surname: 'evaggelou', id: 2 },
+    { name: 'lid' , surname: 'evaggelou', id: 3 },
+    { name: 'kostas' , surname: 'evaggelou', id: 1 },
+    { name: 'nik' , surname: 'evaggelou', id: 2 },
+    { name: 'lid' , surname: 'evaggelou', id: 3 },
+    { name: 'kostas' , surname: 'evaggelou', id: 1 },
+    { name: 'nik' , surname: 'evaggelou', id: 2 },
+    { name: 'lid' , surname: 'evaggelou', id: 3 }
+  ]
   constructor() { }
 
   ngOnInit() {
