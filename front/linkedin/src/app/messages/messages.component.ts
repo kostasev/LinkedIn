47f506be111846a6bfb62169205a839ed0c66362
Kostas Evangelou <kostas.ev79@gmail.com>
2018-09-09 16:58:34 +0300

    efwef
    efwef

--- front/linkedin/src/app/messages/messages.component.ts
+++ front/linkedin/src/app/messages/messages.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  public myId;
   msgUsers = [
     { name: 'kostas' , surname: 'evaggelou', id: 1 },
     { name: 'nik' , surname: 'evaggelou', id: 2 },
@@ -26,9 +27,19 @@ export class MessagesComponent implements OnInit {
     { name: 'nik' , surname: 'evaggelou', id: 2 },
     { name: 'lid' , surname: 'evaggelou', id: 3 }
   ]
+
+  messages = [ {id: 1 , text: 'mpoale amsdmasd asn a fa afa fa fasdfasd '},
+    {id: 2 , text: 'sd asn a fa afa fa fasdfasd '},
+    {id: 1 , text: 'mpoale amsdmasd asn a fa afa fa fasdfasd '},
+    {id: 2 , text: 'sd asn a fa afa fa fasdfasd '},
+    {id: 1 , text: 'mpoale amsdmasd asn a fa afa fa fasdfasd '},
+    {id: 2 , text: 'sd asn a fa afa fa fasdfasd '},
+    {id: 1 , text: 'mpoale amsdmasd asn a fa afa fa fasdfasd '},
+    {id: 2 , text: 'sd asn a fa afa fa fasdfasd '}];
   constructor() { }
 
   ngOnInit() {
+    this.myId = localStorage.getItem('id');
   }
 
 }
--- front/linkedin/src/app/notifications/notifications.component.ts
+++ front/linkedin/src/app/notifications/notifications.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications.component.css']
 })
 export class NotificationsComponent implements OnInit {
+  notifications = [ {name: 'kostas', surname: 'evangelou', iduser: 1, idpost: 2 , action: 'liked' , seen: false},
+    {name: 'kostas', surname: 'evangelou', iduser: 1, idpost: 2 , action: 'commented' , seen: true}];
 
+  connections = [ {name: 'kostas', surname: 'evangelou', iduser: 1},
+    {name: 'kostas2', surname: 'evangelou2', iduser: 2}];
   constructor() { }
 
   ngOnInit() {
